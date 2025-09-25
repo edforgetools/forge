e2ea800f7b1141e09631fc2617220ef8e4cb314f
Edward Brooks <ebro0019@student.monash.edu>
2025-09-25 20:35:29 +1000

    chore: convert to meta/docs repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:8787",
+    },
+  },
+});
